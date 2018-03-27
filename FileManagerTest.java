d193642d32c01125feed0f4d00e07d06f19b4ac6
Nickolas <kmbfm111@gmail.com>
2018-03-27 10:51:32 +0300

    Add files via upload

---
+++ FileManagerTest.java
@@ -0,0 +1,19 @@
+package org.itstep;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileManagerTest {
+
+		@Test
+		public void testReadInfoFromFile() {
+
+		}
+
+		@Test
+		public void testReadMixedInfoFromFile() {
+			
+		}
+		
+}
